fa9a0e919601ae986ac69a4dc3fd4b17759a27cd
Joseph Martinez <josephmartinezcortes5@gmail.com>
2025-03-11 21:37:08 -0600

    Implmentación de comandos

--- ClienteMapacheChat.py
+++ ClienteMapacheChat.py
@@ -4,7 +4,7 @@ import tkinter as tk  # biblioteca apra la interfaz grafia
 from tkinter import simpledialog, scrolledtext
 
 #apuntamos el cliente al servidor con su ip y el grupo (puerto)
-DIRECCION_SERVIDOR = "192.168.106.84"
+DIRECCION_SERVIDOR = "10.0.11.102"
 PUERTO_SERVIDOR = 12345
 
 #COMENTARIO_PRUEBA
